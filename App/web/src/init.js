7e39c7aeb86ee7b6c18fb815ede148f923c3c372
Yitzchak Schwarz <s.y.schwarz@gmail.com>
2021-01-01 01:37:48 +0200

    Make API key private
    moved the firebaseConfig and initialization to a separate file and added it to .gitignore to keep the API key private. Also changed the API key so that the old one won't work anymore.

--- App/web/src/init.js
+++ App/web/src/init.js
@@ -7,20 +7,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/functions";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-	apiKey: "AIzaSyBazKqkG-2RCZYS0JqtIOkVYZU9TvKORaQ",
-	authDomain: "csfpd-da7e7.firebaseapp.com",
-	projectId: "csfpd-da7e7",
-	storageBucket: "csfpd-da7e7.appspot.com",
-	messagingSenderId: "604605933482",
-	appId: "1:604605933482:web:92d68b782d5a32ae0c2dd3",
-	measurementId: "G-P65FTMVX43"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+import "./firebase_config";
 
 export const fb = firebase;
 export const db = firebase.firestore();
