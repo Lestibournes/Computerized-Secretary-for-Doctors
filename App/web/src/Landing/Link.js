d7f8fec92e0b9fac661c4b3a770cce823ce71713
Yitzchak Schwarz <s.y.schwarz@gmail.com>
2021-06-22 17:29:40 +0300

    Fixed Links component

--- App/web/src/Landing/Link.js
+++ App/web/src/Landing/Link.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router"
 import { Header } from "../Common/Components/Header";
 import { Loading } from "../Common/Components/Loading";
+import { usePopups } from "../Common/Popups";
 import { useRoot } from "../Common/Root";
 import { server } from "../Common/server";
+import { db } from "../init";
 import { ClinicLandingFragment } from "./ClinicLandingFragment";
 import { DoctorLandingFragment } from "./DoctorLandingFragment";
 
@@ -12,16 +14,25 @@ const DOCTOR = "doctor";
 
 export function Link() {
 	const root = useRoot();
+	const popups = usePopups();
+
 	const {link} = useParams();
 	const [target, setTarget] = useState();
 
 	useEffect(() => {
 		if (link) {
-			server.links.getTarget({name: link}).then(response => {
-				if (response.data.success) setTarget(response.data.target);
-			});
-
-			root.set(link);
+			db.collection("links").doc(link).get().then(
+				link_snap => {
+					if (link_snap.exists) {
+						const link_data = link_snap.data();
+						link_data.id = link_data.id;
+						setTarget(link_data);
+						root.set(link);
+					}
+					else popups.error("Page doesn't exist");
+				}
+			)
+			.catch(reason => popups.error(reason.message));
 		}
 	}, [link]);
 
